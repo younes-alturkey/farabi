50b5d5618fa8056a005c41ebe7f757fc3ba6227e
Younes Alturkey <hi@younes.ninja>
2023-07-12 23:20:34 +0300

    update api key

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -2,6 +2,6 @@ export const API_URL = 'https://api.openai.com'
 export const APP_VERSION = '1.0.0'
 export const OPENAI_ORG_ID = 'org-doalDuiZ0jmNNLQ5e9ebNcws'
 export const OPENAI_API_KEY =
-    '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'
+    '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'
 export const MODEL_SETTING =
     'Answer my questions as if you were Al Farabi the second teacher and end your answer with a percentage number with 4 asterisks before the number of how accurate you think your answer were'
