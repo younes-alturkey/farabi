e4c21647c59bad7f5793ead6bf6fbc36d45447be
Younes Alturkey <hi@younes.ninja>
2023-07-12 23:27:46 +0300

    remove api key

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,7 +1,6 @@
 export const API_URL = 'https://api.openai.com'
 export const APP_VERSION = '1.0.0'
-export const OPENAI_ORG_ID = 'org-doalDuiZ0jmNNLQ5e9ebNcws'
-export const OPENAI_API_KEY =
-    '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'
+export const OPENAI_ORG_ID = ''
+export const OPENAI_API_KEY = ''
 export const MODEL_SETTING =
     'Answer my questions as if you were Al Farabi the second teacher and end your answer with a percentage number with 4 asterisks before the number of how accurate you think your answer were'
